34d6e0c305fb5f26666c3fc3bfd67a10a77a8a1f
hr955 <nfzoze01@gmail.com>
2021-06-23 22:14:00 +0900

    #3 NewsListPage Category UI 생성

---
+++ app/src/main/java/com/example/taxijjangnews/category/NewsListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.taxijjangnews.category
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.taxijjangnews.R
+
+class CategoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_category)
+    }
+}
